0ad3335d8af7c41bacd4f091953cec6a40ae4f3a
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-08-15 22:15:03 -0300

    chore: init the project color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        blue: {
+          light: "#d1e8ff",
+          primary: "#1c2e63",
+          dark: "##1C2143",
+        },
+        gray: {
+          light: "#f5f5f5",
+          primary: "#e9e4e3",
+          dark: "#a39f9e",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
